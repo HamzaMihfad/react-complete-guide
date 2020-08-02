d5ce12ea7c3f94d960b55484704cbe50b01fcd36
Mihfad Hamza <hamza@ciwa.io>
2020-08-02 10:59:55 +0100

    React Developter Tools & Chrome Debugging Tested!

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [text, setText] = useState('');
+  const [text2, setText2] = useState('');
+
+  const onChangeHandler = (event) => {
+    setText(event.target.value);
+    setText2(event.target.value + 'test');
+    console.log(text);
+  }
+
   return (
     <div className="App">
       <h1>Hellow World!</h1>
+  <h2>{text}</h2>
+      <input onChange={onChangeHandler}/>
     </div>
   );
 }
